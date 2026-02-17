132e0e662c5656453d24d2ae23c0baa3dfde434e
Dylan Emerson <emersonds2868@gmail.com>
2026-02-16 20:49:05 -0500

    Removed handleCommands function in favor of a switch statement in handleWebSocketMessage
    This allows the bot to have access to all of the payload data, rather than the original method of only sending the contents of the message and trimming it.

--- bot.js
+++ bot.js
@@ -28,178 +28,192 @@ var websocketSessionID;
 
 // Start executing the bot from here
 (async () => {
-	// Verify that the authentication is valid
-	await getAuth();
+    // Verify that the authentication is valid
+    await getAuth();
 
-	// Start WebSocket client and register handlers
-	const websocketClient = startWebSocketClient();
+    // Start WebSocket client and register handlers
+    const websocketClient = startWebSocketClient();
 })();
 
 // WebSocket will persist the application loop until you exit the program forcefully
 
 async function getAuth() {
-	// https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
-	let response = await fetch('https://id.twitch.tv/oauth2/validate', {
-		method: 'GET',
-		headers: {
-			'Authorization': 'OAuth ' + OAUTH_TOKEN
-		}
-	});
+    // https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
+    let response = await fetch('https://id.twitch.tv/oauth2/validate', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'OAuth ' + OAUTH_TOKEN
+        }
+    });
 
-	if (response.status != 200) {
-		if (response.status == 401) {
-			refreshOauthToken();
-			getAuth();
-		}
-		else {
-		let data = await response.json();
-		console.error("Token is not valid. /oauth2/validate returned status code " + response.status);
-		console.error(data);
-		process.exit(1);
-		}
-	}
+    if (response.status != 200) {
+        if (response.status == 401) {
+            refreshOauthToken();
+            getAuth();
+        }
+        else {
+            let data = await response.json();
+            console.error("Token is not valid. /oauth2/validate returned status code " + response.status);
+            console.error(data);
+            process.exit(1);
+        }
+    }
 
-	console.log("Validated token.");
-	// Send random socials command if token is valid
-	scheduleSocials();
+    console.log("Validated token.");
+    // Send random socials command if token is valid
+    scheduleSocials();
 }
 
 function startWebSocketClient() {
-	let websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
+    let websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
 
-	websocketClient.on('error', console.error);
+    websocketClient.on('error', console.error);
 
-	websocketClient.on('open', () => {
-		console.log('WebSocket connection opened to ' + EVENTSUB_WEBSOCKET_URL);
-	});
+    websocketClient.on('open', () => {
+        console.log('WebSocket connection opened to ' + EVENTSUB_WEBSOCKET_URL);
+    });
 
-	websocketClient.on('message', (data) => {
-		handleWebSocketMessage(JSON.parse(data.toString()));
-	});
+    websocketClient.on('message', (data) => {
+        handleWebSocketMessage(JSON.parse(data.toString()));
+    });
 
-	return websocketClient;
+    return websocketClient;
 }
 
 function handleWebSocketMessage(data) {
-	switch (data.metadata.message_type) {
-		case 'session_welcome': // First message you get from the WebSocket server when connecting
-			websocketSessionID = data.payload.session.id; // Register the Session ID it gives us
+    switch (data.metadata.message_type) {
+        case 'session_welcome': // First message you get from the WebSocket server when connecting
+            websocketSessionID = data.payload.session.id; // Register the Session ID it gives us
 
-			// Listen to EventSub, which joins the chatroom from your bot's account
-			registerEventSubListeners();
-			break;
-		case 'notification': // An EventSub notification has occurred, such as channel.chat.message
-			switch (data.metadata.subscription_type) {
-				case 'channel.chat.message':
-					// First, print the message to the program's console.
-					console.log(`MSG #${data.payload.event.broadcaster_user_login} <${data.payload.event.chatter_user_login}> ${data.payload.event.message.text}`);
+            // Listen to EventSub, which joins the chatroom from your bot's account
+            registerEventSubListeners();
+            break;
+        case 'notification': // An EventSub notification has occurred, such as channel.chat.message
+            switch (data.metadata.subscription_type) {
+                case 'channel.chat.message':
+                    // First, print the message to the program's console.
+                    console.log(`MSG #${data.payload.event.broadcaster_user_login} <${data.payload.event.chatter_user_login}> ${data.payload.event.message.text}`);
 
-					// "Split" the chat message to get the first word
-					// This is used to check if the message is a valid command
-					// "!contraption EmicoMirari" becomes "!contraption"
-					var newChat = [ data.payload.event.message.text.split(' ')[0], data.payload.event.message.text ];
-					//console.log("Shortened chat: " + newChat);
-					// Then check to see if that message is a command
-					if (COMMANDS_DICTIONARY.has(newChat[0])) {
-						// TODO: Fix command handling to have access to user name/ID
-						handleCommands(newChat);
-					}
+                    // "Split" the chat message to get the first word
+                    // This is used to check if the message is a valid command
+                    // "!contraption EmicoMirari" becomes "!contraption"
+                    var newChat = [data.payload.event.message.text.split(' ')[0], data.payload.event.message.text];
+                    //console.log("Shortened chat: " + newChat);
+                    // Then check to see if that message is a command
+                    if (COMMANDS_DICTIONARY.has(newChat[0])) {
+                        switch (COMMANDS_DICTIONARY.get(newChat[0])) {
+                            case "quote":
+                                // TODO: Add quote command
+                                // This will require setting up a database to keep
+                                // track of all of the quotes and their IDs.
+                                break;
+                            case "contraption":
+                                commandContraption(data.payload.event.message.text);
+                                break;
+                            case "lurk":
+                                commandLurk(chatMessage);
+                                break;
+                            default:
+                                sendChatMessage(output)
+                                break;
+                        }
+                    }
 
-					break;
-			}
-			break;
-	}
+                    break;
+            }
+            break;
+    }
 }
 
 // Responds to a chat command with the expected output
-function handleCommands(chatMessage) {
-	let output = COMMANDS_DICTIONARY.get(chatMessage[0]);
-	console.log("COMMANDS: " + COMMANDS_DICTIONARY.get(chatMessage));
-	console.log("output: " + output);
-	
-	switch (output) {
-		case "quote":
-			// TODO: Add quote command
-			// This will require setting up a database to keep
-			// track of all of the quotes and their IDs.
-			break;
-		case "contraption":
-			commandContraption(chatMessage[1]);
-			break;
-		case "lurk":
-			commandLurk(chatMessage);
-			break;
-		default:
-			sendChatMessage(output)
-			break;
-	}
+function handleCommands(chatMessage, chatter) {
+    let output = COMMANDS_DICTIONARY.get(chatMessage[0]);
+    console.log("COMMANDS: " + COMMANDS_DICTIONARY.get(chatMessage));
+    console.log("output: " + output);
+
+    switch (output) {
+        case "quote":
+            // TODO: Add quote command
+            // This will require setting up a database to keep
+            // track of all of the quotes and their IDs.
+            break;
+        case "contraption":
+            commandContraption(chatMessage[1]);
+            break;
+        case "lurk":
+            commandLurk(chatMessage);
+            break;
+        default:
+            sendChatMessage(output)
+            break;
+    }
 }
 
 async function sendChatMessage(chatMessage) {
-	let response = await fetch('https://api.twitch.tv/helix/chat/messages', {
-		method: 'POST',
-		headers: {
-			'Authorization': 'Bearer ' + OAUTH_TOKEN,
-			'Client-Id': CLIENT_ID,
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			broadcaster_id: CHAT_CHANNEL_USER_ID,
-			sender_id: BOT_USER_ID,
-			message: chatMessage
-		})
-	});
+    let response = await fetch('https://api.twitch.tv/helix/chat/messages', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + OAUTH_TOKEN,
+            'Client-Id': CLIENT_ID,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            broadcaster_id: CHAT_CHANNEL_USER_ID,
+            sender_id: BOT_USER_ID,
+            message: chatMessage
+        })
+    });
 
-	if (response.status != 200) {
-		if (response.stats == 401) {
-			refreshOauthToken();
-			sendChatMessage(chatMessage);
-		} else {
-			let data = await response.json();
-			console.error("Failed to send chat message");
-			console.error(data);
-		}
-	} else {
-		console.log("Sent chat message: " + chatMessage);
-	}
+    if (response.status != 200) {
+        if (response.stats == 401) {
+            refreshOauthToken();
+            sendChatMessage(chatMessage);
+        } else {
+            let data = await response.json();
+            console.error("Failed to send chat message");
+            console.error(data);
+        }
+    } else {
+        console.log("Sent chat message: " + chatMessage);
+    }
 }
 
 async function registerEventSubListeners() {
-	// Register channel.chat.message
-	let response = await fetch('https://api.twitch.tv/helix/eventsub/subscriptions', {
-		method: 'POST',
-		headers: {
-			'Authorization': 'Bearer ' + OAUTH_TOKEN,
-			'Client-Id': CLIENT_ID,
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			type: 'channel.chat.message',
-			version: '1',
-			condition: {
-				broadcaster_user_id: CHAT_CHANNEL_USER_ID,
-				user_id: BOT_USER_ID
-			},
-			transport: {
-				method: 'websocket',
-				session_id: websocketSessionID
-			}
-		})
-	});
+    // Register channel.chat.message
+    let response = await fetch('https://api.twitch.tv/helix/eventsub/subscriptions', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + OAUTH_TOKEN,
+            'Client-Id': CLIENT_ID,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: 'channel.chat.message',
+            version: '1',
+            condition: {
+                broadcaster_user_id: CHAT_CHANNEL_USER_ID,
+                user_id: BOT_USER_ID
+            },
+            transport: {
+                method: 'websocket',
+                session_id: websocketSessionID
+            }
+        })
+    });
 
-	if (response.status != 202) {
-		if (response.status == 401) {
-			refreshOauthToken();
-			registerEventSubListeners();
-		}
-		let data = await response.json();
-		console.error("Failed to subscribe to channel.chat.message. API call returned status code " + response.status);
-		console.error(data);
-		process.exit(1);
-	} else {
-		const data = await response.json();
-		console.log(`Subscribed to channel.chat.message [${data.data[0].id}]`);
-	}
+    if (response.status != 202) {
+        if (response.status == 401) {
+            refreshOauthToken();
+            registerEventSubListeners();
+        }
+        let data = await response.json();
+        console.error("Failed to subscribe to channel.chat.message. API call returned status code " + response.status);
+        console.error(data);
+        process.exit(1);
+    } else {
+        const data = await response.json();
+        console.log(`Subscribed to channel.chat.message [${data.data[0].id}]`);
+    }
 }
 
 
@@ -207,38 +221,38 @@ async function registerEventSubListeners() {
 // Not sure if this is working as intended yet so we are using Implicit tokens instead
 // Send an HTTPS POST request with botData and parses response to .env
 async function refreshOauthToken() {
-	//console.log("Entered refreshOauth");
+    //console.log("Entered refreshOauth");
 
-	// Set up URLParams for POST request
-	let botData = new URLSearchParams({
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		refresh_token: process.env.REFRESH_OAUTH_TOKEN,
-		grant_type: "refresh_token"
-	});
+    // Set up URLParams for POST request
+    let botData = new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_OAUTH_TOKEN,
+        grant_type: "refresh_token"
+    });
 
-	// Try refreshing OAuth token
-	try {
-		// HTTPS POST request
-		let response = await fetch('https://id.twitch.tv/oauth2/token', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: botData.toString()
-		});
+    // Try refreshing OAuth token
+    try {
+        // HTTPS POST request
+        let response = await fetch('https://id.twitch.tv/oauth2/token', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: botData.toString()
+        });
 
-		// Log errors
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}\n${response.message}`);
-		}
+        // Log errors
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}\n${response.message}`);
+        }
 
-		// Parse json and update .env file
-		let json = await response.json();
+        // Parse json and update .env file
+        let json = await response.json();
 
-		setEnvValue("OAUTH_TOKEN", json.access_token);
-		setEnvValue("REFRESH_OAUTH_TOKEN", json.refresh_token);
-	} catch (error) {
-		console.error('Error fetching token:', error);
-	}
+        setEnvValue("OAUTH_TOKEN", json.access_token);
+        setEnvValue("REFRESH_OAUTH_TOKEN", json.refresh_token);
+    } catch (error) {
+        console.error('Error fetching token:', error);
+    }
 }
 
 
@@ -265,73 +279,73 @@ function setEnvValue(key, value) {
 // Gimmick command that "locks" a user in a contraption
 // Just used as a meme in chat, does not actually do anything negative
 function commandContraption(chatMessage) {
-	// Remove "!contraption"
-	const newStr = chatMessage.trim().slice(13);
+    // Remove "!contraption"
+    const newStr = chatMessage.trim().slice(13);
 
-	// Split message to get each trapped user, where " " is the separator
-	const trappedUsers = newStr.split(" ");
+    // Split message to get each trapped user, where " " is the separator
+    const trappedUsers = newStr.split(" ");
 
-	console.log(trappedUsers);
+    console.log(trappedUsers);
 
-	// Final string
-	var output = "";
+    // Final string
+    var output = "";
 
-	// If multiple users are being put in the contraption
-	if (trappedUsers.length > 2) {
-		for (var i = 0; i < trappedUsers.length; i++) {
-			// First user
-			if (i === 0) {
-				console.log('i==0');
-				output += trappedUsers[i] + ", ";
-			}
-			// Last user
-			else if (i === (trappedUsers.length - 1)) {
-				console.log("last user");
-				output += "and " + trappedUsers[i] + " have been thrown into the Contraption™!";
-			}
-			// All other users
-			else {
-				output += trappedUsers[i] + ", ";
-			}
-		}
-		
-	}
-	else if (trappedUsers.length > 1) {
-		output = trappedUsers[0] + " and " + trappedUsers[1] + " have been thrown into the Contraption™!";
-	}
-	else {
-		output = trappedUsers[0] + " has been thrown into the Contraption™!";
-	}
+    // If multiple users are being put in the contraption
+    if (trappedUsers.length > 2) {
+        for (var i = 0; i < trappedUsers.length; i++) {
+            // First user
+            if (i === 0) {
+                console.log('i==0');
+                output += trappedUsers[i] + ", ";
+            }
+            // Last user
+            else if (i === (trappedUsers.length - 1)) {
+                console.log("last user");
+                output += "and " + trappedUsers[i] + " have been thrown into the Contraption™!";
+            }
+            // All other users
+            else {
+                output += trappedUsers[i] + ", ";
+            }
+        }
 
-	sendChatMessage(output);
+    }
+    else if (trappedUsers.length > 1) {
+        output = trappedUsers[0] + " and " + trappedUsers[1] + " have been thrown into the Contraption™!";
+    }
+    else {
+        output = trappedUsers[0] + " has been thrown into the Contraption™!";
+    }
+
+    sendChatMessage(output);
 }
 
 // Sends a thank you message to the lurking user
 function commandLurk(newChat) {
-	// TODO: Update command to use the user's name.
-	// Can't do this until handleCommands is changed to accept
-	// the whole payload instead of just the message.
+    // TODO: Update command to use the user's name.
+    // Can't do this until handleCommands is changed to accept
+    // the whole payload instead of just the message.
 
-	const output = "Thank you for lurking! I appreciate my place on your second monitor emicomHeart";
-	sendChatMessage(output);
+    const output = "Thank you for lurking! I appreciate my place on your second monitor emicomHeart";
+    sendChatMessage(output);
 }
 
 
 // Randomly selects a command to send to chat and repeats every 30 minutes.
 // These commands should be related to other social platforms.
 function scheduleSocials() {
-	const commands = [ "!socials", "!discord"];
-	const randomCommand = Math.floor(Math.random() * commands.length);
-	const selectedCommand = [ commands[randomCommand] ]; // This is an array so selectedCommand[0] is the command instead of the first character in the string
-	const timeToWait = 1800 * 1000; // seconds * 1000 to convert to milliseconds
+    const commands = ["!socials", "!discord"];
+    const randomCommand = Math.floor(Math.random() * commands.length);
+    const selectedCommand = [commands[randomCommand]]; // This is an array so selectedCommand[0] is the command instead of the first character in the string
+    const timeToWait = 1800 * 1000; // seconds * 1000 to convert to milliseconds
 
-	console.log("selectedCommand: " + selectedCommand);
-	console.log("command[0]: " + selectedCommand[0]);
+    console.log("selectedCommand: " + selectedCommand);
+    console.log("command[0]: " + selectedCommand[0]);
 
     handleCommands(selectedCommand);	// Call random socials command
-	
-	// Call this function every thirty minutes
-	setTimeout(() => {
-    scheduleSocials(); // Schedule the next call
-  }, timeToWait);
+
+    // Call this function every thirty minutes
+    setTimeout(() => {
+        scheduleSocials(); // Schedule the next call
+    }, timeToWait);
 }
